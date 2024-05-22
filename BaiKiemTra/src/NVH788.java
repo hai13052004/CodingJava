74e5595b41d53c4eba588a965d352928861e5a7b
unknown <ng.v.hai.000@gmail.com>
2024-05-21 19:47:00 -0700

    NVH788_cau1

---
+++ BaiKiemTra/src/NVH788.java
@@ -0,0 +1,84 @@
+
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class NVH788_Cau1Person {
+    
+    public String name;
+    public int age; 
+    public double height; 
+
+   
+    public NVH788_Cau1Person() {
+    }
+
+    
+    public Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+   
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+  
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void NVH788_Cau2NhapThongTin() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        this.name = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        this.age = scanner.nextInt();
+
+        System.out.print("Enter height: ");
+        this.height = scanner.nextDouble();
+    }
+
+    
+    public void NVH788_Cau2XuatThongTin() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Height: " + this.height);
+    }
+
+  
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        
+        person.input();
+
+      
+        person.display();
+    }
+}
+//xây dựng phương thức thêm mới ,sửa, xóa, hiển thị n phần tử Person lưu trữ rrong ArrayList
+//tạo class Student kế thừa lại class Person và @Override các phương thức trên
+
+public class NVH788 Cau3Arra
