dd269629e4276afc7c284907a2739c220015040a
unknown <ng.v.hai.000@gmail.com>
2024-05-21 19:57:06 -0700

    Update_Code

---
+++ BaiKiemTra/src/NVH788_Cau3ArrayList.java
@@ -0,0 +1,13 @@
+
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class NVH788_Cau1Person {
+    
+    public String name;
+    public int age; 
+    public double height; 
+
+    
+   
