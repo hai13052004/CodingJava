bd96008fb3f38796b96484ade4d83147981d48ac
unknown <ng.v.hai.000@gmail.com>
2024-05-21 19:58:40 -0700

    NVH788_cau2Person

--- BaiKiemTra/src/NVH788_Cau3ArrayList.java
+++ BaiKiemTra/src/NVH788_Cau3ArrayList.java
@@ -10,4 +10,68 @@ class NVH788_Cau1Person {
     public double height; 
 
     
+    public NVH788_Cau2Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+   
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+  
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void NVH788_Cau2NhapThongTin() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name: ");
+        this.name = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        this.age = scanner.nextInt();
+
+        System.out.print("Enter height: ");
+        this.height = scanner.nextDouble();
+    }
+
+    
+    public void NVH788_Cau2XuatThongTin() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Height: " + this.height);
+    }
+
+  
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        
+        person.NVH788_Cau2NhapThongTin();
+
+      
+        person.NVH788_Cau2XuatThongTin();
+    }
+}
+
